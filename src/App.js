3e5480177b99c81c8e7ec18be22bc18b547ed674
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-03-21 16:06:07 +0200

    styles fix

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <div className='flex relative dark:bg-main-dark-bg'>
-                    <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
                         <TooltipComponent content="Settings" position="Top">
                             <button
                                 type='button'
@@ -24,22 +24,21 @@ const App = () => {
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
-                        {activeMenu ? (
-                            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
-                                <Sidebar />
-                            </div>
-
-                        ) : (
-                            <div className='w-0 dark:bg-secondary-dark-bg'>
-                                <Sidebar />
-                            </div>
-                        )}
-                        <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-                            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                                <Navbar />
-                            </div>
+                    </div>
+                    {activeMenu ? (
+                        <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+                            <Sidebar />
                         </div>
 
+                    ) : (
+                        <div className='w-0 dark:bg-secondary-dark-bg'>
+                            <Sidebar />
+                        </div>
+                    )}
+                    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            <Navbar />
+                        </div>
                         <div>
                             <Routes>
                                 {/* Dashboard */}
