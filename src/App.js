76998024d04685ab2261711abb7b291f02112bf6
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-03-23 18:42:25 +0200

    contex and dark theme added

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
 function App() {
-   const {activeMenu} = useStateContext();
+    const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
 
     return (
-        <div>
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <BrowserRouter>
                 <div className="flex relative dark:bg-main-dark-bg">
                     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
@@ -23,7 +23,9 @@ function App() {
                             <button
                                 type='button'
                                 className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                                style={{ background: 'blue', borderRadius: '50%' }}>
+                                style={{ background: currentColor, borderRadius: '50%' }}
+                                onClick={() => setThemeSettings(true)}
+                            >
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
@@ -38,11 +40,18 @@ function App() {
                             <Sidebar />
                         </div>
                     )}
-                    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+                    <div
+                        className={
+                            activeMenu
+                                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                        }
+                    >
                         <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                             <Navbar />
                         </div>
                         <div>
+                            {themeSettings && (<ThemeSettings />)}
                             <Routes>
                                 {/* Dashboard */}
                                 <Route path='/' element={<Ecommerce />} />
