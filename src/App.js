f5700918f1cc3f9483e55e55e1058e98d93dc248
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-03-26 18:31:45 +0300

    pie chart type error fix

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
                                 <Route path='/line' element={<Line />} />
                                 <Route path='/area' element={<Area />} />
                                 <Route path='/bar' element={<Bar />} />
-                                <Route path='/bar' element={<Pie />} />
+                                <Route path='/pie' element={<Pie />} />
                                 <Route path='/financial' element={<Financial />} />
                                 <Route path='/color-mapping' element={<ColorMapping />} />
                                 <Route path='/pyramid' element={<Pyramid />} />
