a5d2b7478f62e72343a451b421635b068563bb06
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2023-03-27 14:21:59 +0300

    Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
-function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+import React from 'react';
 
-export default Footer
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2023 All rights reserved by Admin.com
+    </p>
+  </div>
+);
+
+export default Footer;
